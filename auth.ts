f732a830c0e200a352a0221d32c351bc927bb437
Kyle Marco Cabiling <kylemarcocabiling@gmail.com>
2025-03-23 13:57:52 +0800

    Added secret key

--- auth.ts
+++ auth.ts
@@ -3,4 +3,5 @@ import Google from "next-auth/providers/google"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
+  secret: "e6380966905a9224b2a25abdbe32769c",
 })
