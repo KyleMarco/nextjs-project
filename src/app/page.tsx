17599380fc87485bf410314e7628499827a96d94
Kyle Marco Cabiling <kylemarcocabiling@gmail.com>
2025-03-23 09:20:51 +0800

    Fixed build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import LinkButton from "@/app/components/link-button";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center p-4">
-      <Button href="/home" label="Start here!" />
+      <LinkButton href="/home" label="Start here!" />
     </div>
   );
 }
